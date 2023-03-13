a60ab5798f3fa3bda996a97243df5bd656e83222
unknown <vincent.vuagnat@orange.fr>
2023-03-13 06:58:52 +0100

    a

--- HTTPServer/index.php
+++ HTTPServer/index.php
@@ -1,9 +1,37 @@
 <!DOCTYPE html>  
      <head>  
       <title>Hello World!</title>
-     </head>  
+     </head>   
 
      <body>  
-      <h1>Hello World!</h1>
-      <p><?php echo 'We are running PHP, version: ' . phpversion(); ?></p>
-     </body>
+      <h1>Hello World!</h1>  
+      <p><?php echo 'We are running PHP, version: ' . phpversion(); ?></p>  
+      <?  
+       $database ="classicmodels";  
+       $user = "root";  
+       $password = "vincent";  
+       $host = "192.168.80.122:8000";  
+
+       //$connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);  
+       $conn = mysqli_connect($host, $user, $password, $database);
+
+       #$query = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE='BASE TABLE'");  
+       #$tables = $query->fetchAll(PDO::FETCH_COLUMN);  
+
+       $req = "SELECT customerName FROM customers";
+       $res = "$conn->query($req)";
+       $tables = mysqli_fetch_array($res);
+
+        if (empty($tables)) {
+          echo "<p>There are no tables in database \"{$database}\".</p>";
+        } else {
+          echo "<p>Database \"{$database}\" has the following tables:</p>";
+          echo "<ul>";
+            foreach ($tables as $table) {
+              echo "<li>{$table}</li>";
+            }
+          echo "</ul>";
+        }
+        ?>
+    </body>
+</html>
